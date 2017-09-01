275207757714a722dd3345df34d4978036feb60c
eramirez20 <esteban.ramirezo@udea.edu.co>
2017-09-01 14:44:14 -0500

    Practica 1 punto 5

---
+++ app/src/main/java/com/esteban_ramirez_osorio/practica1_punto51/MainActivity.java
@@ -0,0 +1,13 @@
+package com.esteban_ramirez_osorio.practica1_punto51;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
